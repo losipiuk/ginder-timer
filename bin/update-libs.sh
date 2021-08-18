f962dde255786f558fdf1a359c5d9df1502ac049
Łukasz Osipiuk <lukasz@osipiuk.net>
2021-08-18 22:01:39 +0200

    Add update-libs.sh script

---
+++ bin/update-libs.sh
@@ -0,0 +1,25 @@
+#/usr/bin/env bash
+
+set -euo pipefail
+
+PROJECT_NAME=grindertimer
+LIBS_FOLDER=${PROJECT_NAME}
+
+updateLib() {
+  local repo=$1
+  local branch=$2
+  shift 2
+  local files=$*
+  local libName=$(basename ${repo})
+  echo "Updating library $libName"
+
+  for file in ${files}; do
+    curl -s https://raw.githubusercontent.com/${repo}/${branch}/${file} -o ${LIBS_FOLDER}/$(basename ${file})
+  done
+}
+
+updateLib Seeed-Studio/Grove_4Digital_Display master TM1637.h TM1637.cpp
+updateLib ArduinoGetStarted/button master src/ezButton.h src/ezButton.cpp
+
+# do some patching :)
+sed -i 's/#include <ezButton.h>/#include "ezButton.h"/' ${LIBS_FOLDER}/ezButton.cpp
